e4f87f6497977ea431457c32e646cbc7ddef67d4
patoglu <ypatoglu@icloud.com>
2021-08-04 20:58:18 +0300

    leetcode ch1

---
+++ leetcode/removeduplicatesfromsortedarray.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index = 1;
+        if(nums.size() == 0) return 0;
+        for(int i = 0 ; i < nums.size() - 1 ; ++i)
+        {
+            if(nums[i] != nums[i + 1])
+            {
+                nums[index++] = nums[i + 1];
+            }
+        }
+        return index;
+        
+    }
+    
+};
