c5588c8f1e4db8dc43361383fb45f4af77b527dd
patoglu <ypatoglu@icloud.com>
2021-08-06 16:44:44 +0300

    pitfall

---
+++ pitfalls/ordermatters.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player
+{
+    
+
+private:
+    string m_jersey, m_first_name, m_last_name;
+public:
+    Player(string first_name, string last_name):
+    m_first_name(first_name), m_last_name(last_name), m_jersey(first_name + " 9 " + last_name){};
+    void print()const
+    {
+        cout << m_jersey << endl;
+    }
+};
+
+int main()
+{
+    Player zlatan("Zlatan", "Ibrahimovic");
+    zlatan.print();
+}
